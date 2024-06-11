557ef1879581b4adb869a5d2a0aa697dbbc85fc3
Jeqaa <j.torsandy@gmail.com>
2024-06-11 13:23:46 +0700

    rapihin dashboard admin

---
+++ resources/views/layoutslte/template.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('layoutslte.header');
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+        @include('layoutslte.navbar');
+        @include('layoutslte.sidebar');
+        @yield('content');
+
+        @include('layoutslte.script')
+    </div>
+</body>
+</html>
+  
