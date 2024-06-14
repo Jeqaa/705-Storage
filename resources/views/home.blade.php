dbd816b85a395792d82c62d3ee9862c67774f4e3
Shinzi <shinzisaja@gmail.com>
2024-06-14 22:52:22 +0700

    AUTH WITH LARAVEL/UI, FOR ENV CHANGES PLS CONTACT

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,37 +1,23 @@
-@extends('layouts.template');
-
-@section('title')
-Home
-@endsection
+@extends('layouts.app')
 
 @section('content')
-    <!-- Main Content -->
-    <div class="container-fluid">
-      <h1>{{ $title }}</h1>
-      <h3 class="pt-1 mb-3">All Items</h3>
-      <div class="row">
-        <table class="table table-striped">
-          <thead class="table-head">
-            <tr>
-              <th>Name</th>
-              <th>Category</th>
-              <th>Stock</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr class="itemsRow">
-              <td>namanyaaaaaaaaa</td>
-              <td>categorynyaaaaaaa</td>
-              <td>101</td>
-              <td class="tdBtn">
-                <button class="editBtn me-4 rounded-2">Edit</button>
-                <button class="deleteBtn rounded-2">Delete</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -8,18 +8,31 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider and will be assigned to
+| the "web" middleware group. Create something great!
 |
 */
 
 Route::get('/', function () {
-    return view('home' , ['title' => 'Home']);
+    return view('home2', ['title' => 'Home']);
 });
 
-Route::get('/history', function () {
-    return view('history', ['title' => 'History']);
+// Route untuk authetikasi, jadi gak usah verify email, user bisa akses ini
+Route::middleware(['auth'])->group(function () {
+    
 });
-Route::get('/setting', function () {
-    return view('setting', ['title' => 'Setting']);
+
+// Route untuk authetikasi yang harus verifikasi email.
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/history', function () {
+        return view('history', ['title' => 'History']);
+    })->name('history');
+    Route::get('/setting', function () {
+        return view('setting', ['title' => 'Setting']);
+    })->name('setting');
+    // TAMBAHIN ROUTE LAIN
+    // Route::get
 });
+Auth::routes(['verify'=>true]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
