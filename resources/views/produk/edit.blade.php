a2b2f4141db310a8cb3fe53035de827027ced71a
Jeqaa <j.torsandy@gmail.com>
2024-06-17 23:08:33 +0700

    rapihin bagian edit

--- resources/views/produk/edit.blade.php
+++ resources/views/produk/edit.blade.php
@@ -1,16 +1,21 @@
+@extends('layoutslte.template')
 @section('content')
-    <section class = "page-section portfolio" id ="tambah">
-        <div class="container">
-            <h1>Edit Produk</h1>
+    <section class="content">
+        <div class="container-fluid">
 
-            <form action="{{ route('produk.update', $prd->id) }}" method="POST">
+
+            <form id="myEditForm" class="col-md-6" action="{{ route('produk.update', $prd->id) }}" method="POST">
+                <div class="card-header d-flex justify-content-center border-bottom mb-3">
+                    <h3 class="card-title py-3 fs-4 fw-bold">EDIT ITEM</h3>
+                </div>
                 @method('PUT')
                 @csrf
-                <div class="mb-3">
-                    <label for="nama_produk">Nama Produk:</label>
-                    <input type="nama_produk" name="nama_produk" id="nama_produk" value="{{ $prd->nama_produk }}" required>
+                <div class="form-group">
+                    <label for="nama_produk" class="form-label">Nama Produk:</label>
+                    <input type="nama_produk" class="form-control" name="nama_produk" id="nama_produk"
+                        value="{{ $prd->nama_produk }}" required>
                 </div>
-                <div class="mb-3">
+                <div class="form-group">
                     <label for="kategori" class="form-label">Kategori</label>
                     <select class="form-select" id="kategori" name="kategori">
                         <option value="Best Seller">Best Seller</option>
@@ -18,8 +23,8 @@
                         <!-- Tambahkan opsi kategori lainnya sesuai kebutuhan -->
                     </select>
                 </div>
-                <div class="mb-3">
-                    <label for="jumlah_barang">Jumlah_barang:</label>
+                <div class="form-group">
+                    <label for="jumlah_barang" class="form-label">Jumlah_barang:</label>
                     <div class="input-group">
                         <!-- <button class="btn btn-outline-secondary" type="button" id="btnMinus">-</button> -->
                         <input type="number" name="jumlah_barang" id="jumlah_barang" class="form-control input-number"
@@ -29,7 +34,11 @@
                 </div>
 
 
-                <button type="submit" class ="btn btn-primary">Submit</button>
+                <div class="form-group d-flex justify-content-between">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <a href="{{ route('produk') }}" type="button" class="btn btn-secondary">Cancel</a>
+                </div>
             </form>
         </div>
     </section>
+@endsection
